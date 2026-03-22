bd435ec2df3353470b00be2c773b3644dfe289fb
Serafimavicius <redas.serafimavicius@dfinsolutions.com>
2026-03-22 13:01:53 +0200

    Add gitignore. Remove unnecessary files

--- backend/build/resources/main/db/migration/V1__init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE players (
-    id      UUID PRIMARY KEY,
-    game_id UUID         NOT NULL,
-    name    VARCHAR(255) NOT NULL,
-    avatar  VARCHAR(1) NOT NULL ,
-    score   INTEGER      NOT NULL DEFAULT 0
-);
-
